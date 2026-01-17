81534544fa7fab5553ffbf7f82b265949ca6d657
DNishadini <dinushikaherath150@gmail.com>
2026-01-17 22:23:13 +0530

    Styles 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import Weather from "./components/Weather";
 import UserStatus from "./components/UserStatus";
 import Greeting from "./components/Greeting";
+import StyleCard from "./components/styles/StyleCard";
 
 const App = () => {
   return (
     <div>
-      <Weather temp={26} />
-      <UserStatus loggedIn={true} isAdmin={false} />
-      <Greeting timeOfDay="morning" />
+      <StyleCard />
     </div>
   );
 };
---
+++ src/components/styles/StyleCard.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function StyleCard() {
+  return (
+    <div>
+      <h1
+        style={{
+          backgroundColor: "lightblue",
+          padding: "20px",
+          borderRadius: "10px",
+          color: "darkblue",
+        }}
+      >
+        Style Cars
+      </h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio magnam
+        obcaecati, perferendis assumenda, modi numquam molestiae quod, id
+        nostrum quasi expedita? Praesentium iste ullam explicabo eius doloremque
+        consequuntur repellat minus!
+      </p>
+    </div>
+  );
+}
+
+export default StyleCard;
